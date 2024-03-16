786230e8d903fccc334a11a2439cdf0ef9f6dce9
Italo Canturil <italo.cantur@gmail.com>
2024-03-16 12:48:47 -0300

    create `DropZone` and `FilePreview` atom components

---
+++ src/components/atom/DropZone.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :data-active="active" @dragenter.prevent="setActive" @dragover.prevent="setActive" @dragleave.prevent="setInactive" @drop.prevent="onDrop">
+    <slot :dropZoneActive="active"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const emit = defineEmits(['files-dropped']);
+
+let active = ref(false);
+let inActiveTimeout: any = null;
+
+const setActive = () => {
+  active.value = true
+  clearTimeout(inActiveTimeout)
+}
+
+const setInactive = () => {
+  inActiveTimeout = setTimeout(() => {
+    active.value = false
+  }, 50);
+}
+
+const onDrop = (e: Event) => {
+  emit('files-dropped', [...e.dataTransfer.files]);
+}
+
+function preventDefaults(e: Event) {
+    e.preventDefault();
+}
+
+const events = ['dragenter', 'dragover', 'dragleave', 'drop'];
+
+onMounted(() => {
+  events.forEach(eventName => {
+    document.body.addEventListener(eventName, preventDefaults);
+  });
+})
+
+onUnmounted(() => {
+  events.forEach(eventName => {
+    document.body.removeEventListever(eventName, preventDefaults)
+  });
+})
+</script>
---
+++ src/compositions/file-lists.ts
@@ -0,0 +1,39 @@
+import { ref } from 'vue'
+
+export default function () {
+  const files = ref<File[]>([])
+
+  function addFiles(newFiles: any) {
+    const newUploadableFiles = [...newFiles]
+      .map((file) => new UploadableFiles(file))
+      .filter((file) => !fileExists(file.id))
+
+    files.value = files.value.concat(newUploadableFiles)
+  }
+
+  function fileExists(otherId: any) {
+    return files.value.some((id) => id === otherId)
+  }
+
+  function removeFile(file: any) {
+    const index = files.value.indexOf(file)
+
+    if (index > -1) files.value.splice(index, 1)
+  }
+
+  return { files, addFiles, removeFile }
+}
+
+class UploadableFiles {
+  file: File
+  id: string
+  url: string
+  status: null
+
+  constructor(file: File) {
+    this.file = file
+    this.id = `${file.name}-${file.size}-${file.lastModified}-${file.type}`
+    this.url = URL.createObjectURL(file)
+    this.status = null
+  }
+}
