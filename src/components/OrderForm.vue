52b265f2c05b53a7fc6f0a3825372e62ca65e96d
Italo Canturil <italo.cantur@gmail.com>
2024-03-15 22:37:46 -0300

    unit tests to input validations

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -1,5 +1,48 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+let titleError = ref('');
+let flavorError = ref('');
+
+const formRef = ref({
+  title: '',
+  flavor: '',
+  price: null,
+  description: '',
+  img: ''
+})
+
+const handleTitleError = () => {
+  titleError.value = '';
+
+  if (formRef.value.title === null) {
+    titleError.value = "O título é obrigatório";
+  }
+
+  if (formRef.value.title.length < 3) {
+    titleError.value = "O título deve ter no mínimo 3 caracteres";
+  }
+
+  if (formRef.value.title.length > 60) {
+    titleError.value = "O título não deve exceder 60 caracteres.";
+  }
+}
+
+const handleFlavorError = () => {
+  flavorError.value = '';
+
+  if (formRef.value.flavor === null) {
+    flavorError.value = "O título é obrigatório";
+  }
+
+  if (formRef.value.flavor.length < 3) {
+    flavorError.value = "O título deve ter no mínimo 3 caracteres";
+  }
+
+  if (formRef.value.flavor.length > 60) {
+    flavorError.value = "O título não deve exceder 60 caracteres.";
+  }
+}
 </script>
 
 <template>
@@ -7,20 +50,24 @@
     <div class="form__first-row">
       <div>
         <label for="title-order">Título do pedido <span aria-label="required">*</span></label>
-        <input id="title-order" type="text">
+        <input v-model="formRef.title" id="title-order" type="text" @change="handleTitleError()" required title="O título deve ter minimo de 3 caracteres e máximo de 60" minlength="3" maxlength="60">
+
+        <span class="error-message" v-if="titleError.length > 0">{{ titleError }}</span>
       </div>
       <div>
         <label for="flavor">Sabor <span aria-label="required">*</span></label>
-        <input id="flavor" type="text">
+        <input v-model="formRef.flavor" id="flavor" type="text" @change="handleFlavorError()" required title="O sabor deve ter minimo de 3 caracteres e máximo de 60" minlength="3" maxlength="60">
+        
+        <span class="error-message" v-if="flavorError.length > 0">{{ flavorError }}</span>
       </div>
       <div>
         <label for="price">R$ <span aria-label="required">*</span></label>
-        <input id="price" type="number">
+        <input v-model="formRef.price" id="price" type="number" required title="O preço deve ser preenchido">
       </div>
     </div>
     
     <div class="form__second-row">
-      <input type="text">
+      <input v-model="formRef.description" id="description" type="text">
     </div>
     
     <div class="form__third-row">
--- src/components/__tests__/OrderForm.spec.ts
+++ src/components/__tests__/OrderForm.spec.ts
@@ -4,5 +4,36 @@ import { mount } from '@vue/test-utils'
 import OrderForm from '../OrderForm.vue'
 
 describe('OrderForm', () => {
-  it('renders properly', () => {})
+  it('input title validations', async () => {
+    const wrapper = mount(OrderForm)
+    const titleInput = wrapper.find('#title-order')
+    // Title and flavor required, min 3 max 60
+    await titleInput.setValue('ab')
+    expect(wrapper.find('.error-message').exists()).toBe(true)
+
+    await titleInput.setValue('abc')
+    expect(wrapper.find('.error-message').exists()).toBe(false)
+
+    await titleInput.setValue('abc'.repeat(55))
+    expect(wrapper.find('.error-message').exists()).toBe(true)
+
+    await titleInput.setValue('ac'.repeat(30))
+    expect(wrapper.find('.error-message').exists()).toBe(false)
+  }),
+    it('input validations', async () => {
+      const wrapper = mount(OrderForm)
+      const flavorInput = wrapper.find('#flavor')
+      // Title and flavor required, min 3 max 60
+      await flavorInput.setValue('ab')
+      expect(wrapper.find('.error-message').exists()).toBe(true)
+
+      await flavorInput.setValue('abc')
+      expect(wrapper.find('.error-message').exists()).toBe(false)
+
+      await flavorInput.setValue('abc'.repeat(55))
+      expect(wrapper.find('.error-message').exists()).toBe(true)
+
+      await flavorInput.setValue('ac'.repeat(30))
+      expect(wrapper.find('.error-message').exists()).toBe(false)
+    })
 })
